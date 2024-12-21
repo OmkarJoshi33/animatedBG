75521677e360554146b3949b4b477bc69ff39ca5
omkar <omkarjoshi1231@gmail.com>
2024-12-21 15:36:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="div1"></div>
+      <div className="div2"></div>
+      <div className="div3"></div>
+      <div className="div4"></div>
+      <div className="div5"></div>
+      <div className="div6"></div>
+      <div className="div6"></div>
+      <div className="div7"></div>
+      <div className="div8"></div>
+      <div className="div9"></div>
+      <div className="div10"></div>
+      <div className="div11"></div>
+      <div className="div12"></div>
+      <div className="div13"></div>
+      <div className="div14"></div>
+      <div className="div15"></div>
+      <div className="div16"></div>
+      <div className="div17"></div>
+      <div className="div18"></div>
+      <div className="div19"></div>
+      <div className="div20"></div>
+      <div className="div21"></div>
+      <div className="div22"></div>
+      <div className="div23"></div>
+      <div className="div24"></div>
+      <div className="div25"></div>
+      <div className="div26"></div>
+      <div className="div27"></div>
+      <div className="div28"></div>
+      <div className="div29"></div>
+      <div className="div30"></div>
+      <div className="div31"></div>
+      <div className="div32"></div>
+      <div className="div33"></div>
+      <div className="div34"></div>
+      <div className="div35"></div>
+      <div className="div36"></div>
+      <div className="div37"></div>
+      <div className="div38"></div>
+
+      <div className="div39"></div>
+
+      <div className="div40"></div>
+      <div className="div41"></div>
+      <div className="div42"></div>
+      <div className="div43"></div>
+      <div className="div44"></div>
+      <div className="div45"></div>
+      <div className="div46"></div>
+
+      <div className="div47"></div>
+
+      <div className="div48"></div>
+
+      <div className="div49"></div>
+
+      <div className="div50"></div>
+
+      <div className="div51"></div>
+
+      <div className="div52"></div>
+
+      <div className="div53"></div>
+
+      <div className="div54"></div>
+
+      <div className="div55"></div>
+
+      <div className="div56"></div>
+
+      <div className="div57"></div>
+
+      <div className="div58"></div>
+
+      <div className="div59"></div>
+
+      <div className="div60"></div>
+
+      <div className="div61"></div>
+
+      <div className="div61"></div>
+
+
+<div className="div62"></div>
+    </>
+  );
+}
+
+export default App;
